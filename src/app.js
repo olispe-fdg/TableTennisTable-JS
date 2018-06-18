dc902d72eccd85057f76f06f0f1b256d4dc2e651
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2018-06-18 09:55:14 +0100

    Extract functions to simplify command parsing

--- src/app.js
+++ src/app.js
@@ -5,30 +5,42 @@ const InvalidArgumentException = require('./invalid_argument_exception');
 exports.startGame = function (initialLeague) {
   let league = initialLeague;
 
+  function recordWin(command) {
+    const regex = /record win (\w*) (\w*)/;
+    const found = command.match(regex);
+    const winner = found[1];
+    const loser = found[2];
+
+    league.recordWin(winner, loser);
+  }
+
+  function save(command) {
+    const regex = /save \"(.*)\"$/
+    const filename = command.match(regex)[1];
+    fileService.save(filename, league);
+  }
+
+  function load(command) {
+    const regex = /load \"(.*)\"$/
+    const filename = command.match(regex)[1];
+    league = fileService.load(filename);
+  }
+
   return {
     sendCommand: function (command) {
       try {
         if (command.startsWith('add player')) {
           league.addPlayer(command.slice(11));
         } else if (command.startsWith('record win')) {
-          const regex = /record win (\w*) (\w*)/;
-          const found = command.match(regex);
-          const winner = found[1];
-          const loser = found[2];
-
-          league.recordWin(winner, loser);
+          recordWin(command);
         } else if (command === 'print') {
           return leagueRenderer.render(league);
         } else if (command === 'winner') {
           return league.getWinner();
         } else if (command.startsWith('save')) {
-          const regex = /save \"(.*)\"$/
-          const filename = command.match(regex)[1];
-          fileService.save(filename, league);
+          save(command);
         } else if (command.startsWith('load')) {
-          const regex = /load \"(.*)\"$/
-          const filename = command.match(regex)[1];
-          league = fileService.load(filename);
+          load(command);
         } else {
           return `Unknown command "${command}"`;
         }
