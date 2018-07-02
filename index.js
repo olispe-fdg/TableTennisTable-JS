0e8b307122271b25643c3830f3d674adac29ebd2
Suzanne Hamilton <suzanne.hamilton@softwire.com>
2018-07-02 12:01:46 +0100

    Print available commands when the league is loaded

--- index.js
+++ index.js
@@ -5,7 +5,38 @@ const readline = require('readline-sync');
 const league = gameState.createLeague();
 const game = app.startGame(league);
 
-console.log('Welcome to the table tennis table');
+console.log(`Welcome to the table tennis table
+=================================
+
+Commands:
+- add player <name>
+  Example:
+    add player Alice
+
+- record win <winner> <loser>
+  Example:
+    record win Alice Bob
+
+- print
+    Prints the current league
+
+- winner
+    Prints the name of the player at the top of the league
+
+- save <filepath>
+    Save the current league to a JSON file
+  Examples:
+    save my_league.json
+    save ~/some/directory/my_league.json
+
+- load <filepath>
+    Load a saved league from a JSON file
+  Examples:
+    load my_league.json
+    load ~/some/directory/my_league.json
+
+- quit
+`);
 
 let isGameActive = true;
 
