fdfebdeb2d4038dd4f4c2082d274f86d4baaa5db
Oliver Spence <115625185+olispe-fdg@users.noreply.github.com>
2023-09-20 11:04:16 +0100

    Fix eslint config and update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,14 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "mocha": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "eqeqeq": [
-            "error",
-            "always"
-        ],
-        "indent": [
-            "error",
-            2,
-            {
-              "ignoredNodes": ["TemplateLiteral"]
-            }
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "no-console": 0,
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "space-before-function-paren": [
-            "error",
-            "always"
-        ]
-    }
+	env: {
+		es6: true,
+		mocha: true,
+		node: true,
+	},
+	extends: ["eslint:recommended", "prettier"],
+	parserOptions: {
+		sourceType: "module",
+	},
+	rules: {
+		"no-console": 0,
+	},
 };
