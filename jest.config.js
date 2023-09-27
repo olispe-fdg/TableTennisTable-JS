5e16da56f90ccd8b9d63ac184fa877ba7bff3afb
Oliver Spence <115625185+olispe-fdg@users.noreply.github.com>
2023-09-27 10:29:47 +0100

    Update app test to Jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 /** @type {import("jest").Config} */
 const config = {
-	testMatch: ["<rootDir>/test/league_test.js", "<rootDir>/test/league_renderer_test.js"],
+	testMatch: [
+		"<rootDir>/test/league_test.js",
+		"<rootDir>/test/league_renderer_test.js",
+		"<rootDir>/test/app_test.js",
+	],
 	setupFilesAfterEnv: ["jest-extended/all"],
 };
 
--- test/app_test.js
+++ test/app_test.js
@@ -1,18 +1,18 @@
-require('mocha-sinon');
-const chai = require('chai');
-const expect = chai.expect;
+const app = require("../src/app");
+const gameState = require("../src/league");
+const leagueRenderer = require("../src/league_renderer");
+const { when } = require("jest-when");
 
-const app = require('../src/app');
-const gameState = require('../src/league');
-const leagueRenderer = require('../src/league_renderer');
+describe("app command processing", function () {
+	test("prints the current state of the league", function () {
+		const rendered = "rendered league";
+		const league = gameState.createLeague();
+		const renderLeague = jest.spyOn(leagueRenderer, "render");
+		when(renderLeague).calledWith(league).mockReturnValue(rendered);
 
-describe('app command processing', function () {
-  it('prints the current state of the league', function () {
-    const league = gameState.createLeague();
-    const renderLeague = this.sinon.stub(leagueRenderer, 'render');
-    renderLeague.withArgs(league).returns('rendered league');
+		const game = app.startGame(league);
+		const response = game.sendCommand("print");
 
-    const game = app.startGame(league);
-    expect(game.sendCommand('print')).to.equal('rendered league');
-  });
+		expect(response).toBe(rendered);
+	});
 });
