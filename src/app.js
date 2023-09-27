c497e98ea3ecd713bcf0918cbcf6f4cd8ea9e3a4
Oliver Spence <115625185+olispe-fdg@users.noreply.github.com>
2023-09-27 11:33:53 +0100

    Add autosaving on adding players and recording wins

--- src/app.js
+++ src/app.js
@@ -1,55 +1,64 @@
-const fileService = require('./file_service');
-const leagueRenderer = require('./league_renderer');
-const InvalidArgumentException = require('./invalid_argument_exception');
+const uuid = require("uuid");
+const fileService = require("./file_service");
+const leagueRenderer = require("./league_renderer");
+const InvalidArgumentException = require("./invalid_argument_exception");
 
 exports.startGame = function (initialLeague) {
-  let league = initialLeague;
+	let league = initialLeague;
+	let gameId = uuid.v4();
 
-  function recordWin (command) {
-    const regex = /record win (\w*) (\w*)/;
-    const found = command.match(regex);
-    const winner = found[1];
-    const loser = found[2];
+	const autoSave = () => {
+		const path = `./saved_games/${gameId}.json`;
+		fileService.save(path, league);
+	};
 
-    league.recordWin(winner, loser);
-  }
+	function recordWin(command) {
+		const regex = /record win (\w*) (\w*)/;
+		const found = command.match(regex);
+		const winner = found[1];
+		const loser = found[2];
 
-  function save (command) {
-    const regex = /save (.*)$/;
-    const filename = command.match(regex)[1];
-    fileService.save(filename, league);
-  }
+		league.recordWin(winner, loser);
+		autoSave();
+	}
 
-  function load (command) {
-    const regex = /load (.*)$/;
-    const filename = command.match(regex)[1];
-    league = fileService.load(filename);
-  }
+	function save(command) {
+		const regex = /save (.*)$/;
+		const filename = command.match(regex)[1];
+		fileService.save(filename, league);
+	}
 
-  return {
-    sendCommand: function (command) {
-      try {
-        if (command.startsWith('add player')) {
-          league.addPlayer(command.slice(11));
-        } else if (command.startsWith('record win')) {
-          recordWin(command);
-        } else if (command === 'print') {
-          return leagueRenderer.render(league);
-        } else if (command === 'winner') {
-          return league.getWinner();
-        } else if (command.startsWith('save')) {
-          save(command);
-        } else if (command.startsWith('load')) {
-          load(command);
-        } else {
-          return `Unknown command "${command}"`;
-        }
-      } catch (e) {
-        if (e instanceof InvalidArgumentException) {
-          return e.message;
-        }
-        throw e;
-      }
-    }
-  };
+	function load(command) {
+		const regex = /load (.*)$/;
+		const filename = command.match(regex)[1];
+		league = fileService.load(filename);
+	}
+
+	return {
+		sendCommand: function (command) {
+			try {
+				if (command.startsWith("add player")) {
+					league.addPlayer(command.slice(11));
+					autoSave();
+				} else if (command.startsWith("record win")) {
+					recordWin(command);
+				} else if (command === "print") {
+					return leagueRenderer.render(league);
+				} else if (command === "winner") {
+					return league.getWinner();
+				} else if (command.startsWith("save")) {
+					save(command);
+				} else if (command.startsWith("load")) {
+					load(command);
+				} else {
+					return `Unknown command "${command}"`;
+				}
+			} catch (e) {
+				if (e instanceof InvalidArgumentException) {
+					return e.message;
+				}
+				throw e;
+			}
+		},
+	};
 };
