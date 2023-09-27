f6cad109ade9d2f6fb75614369008806c13203ef
Oliver Spence <115625185+olispe-fdg@users.noreply.github.com>
2023-09-27 10:34:46 +0100

    Add tests for adding players

--- test/app_test.js
+++ test/app_test.js
@@ -3,16 +3,48 @@ const gameState = require("../src/league");
 const leagueRenderer = require("../src/league_renderer");
 const { when } = require("jest-when");
 
-describe("app command processing", function () {
-	test("prints the current state of the league", function () {
-		const rendered = "rendered league";
+test("prints the current state of the league", function () {
+	const rendered = "rendered league";
+	const league = gameState.createLeague();
+	const renderLeague = jest.spyOn(leagueRenderer, "render");
+	when(renderLeague).calledWith(league).mockReturnValue(rendered);
+
+	const game = app.startGame(league);
+	const response = game.sendCommand("print");
+
+	expect(response).toBe(rendered);
+});
+
+describe("adding players", () => {
+	test("adds player with name given in command", () => {
+		const playerName = "Alice";
 		const league = gameState.createLeague();
-		const renderLeague = jest.spyOn(leagueRenderer, "render");
-		when(renderLeague).calledWith(league).mockReturnValue(rendered);
+		const addPlayer = jest.spyOn(league, "addPlayer");
 
 		const game = app.startGame(league);
-		const response = game.sendCommand("print");
+		game.sendCommand(`add player ${playerName}`);
 
-		expect(response).toBe(rendered);
+		expect(addPlayer).toHaveBeenCalledWith(playerName);
+	});
+
+	test("handles command without player name", () => {
+		const league = gameState.createLeague();
+		const addPlayer = jest.spyOn(league, "addPlayer");
+
+		const game = app.startGame(league);
+		game.sendCommand(`add player`);
+
+		expect(addPlayer).toHaveBeenCalledWith("");
+	});
+
+	// App's command processing isn't robust
+	test.skip("does not add player when command doesn't match", () => {
+		const league = gameState.createLeague();
+		const addPlayer = jest.spyOn(league, "addPlayer");
+
+		const game = app.startGame(league);
+		game.sendCommand(`add players Alice`);
+
+		expect(addPlayer).not.toHaveBeenCalled();
 	});
 });
