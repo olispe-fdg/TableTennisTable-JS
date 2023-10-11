0de007efce56ea0411782428bdeba6ddf490b61f
Oliver Spence <115625185+olispe-fdg@users.noreply.github.com>
2023-10-11 10:23:16 +0100

    Add integration tests for game

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
 /** @type {import("jest").Config} */
 const config = {
-	testMatch: [
-		"<rootDir>/test/league_test.js",
-		"<rootDir>/test/league_renderer_test.js",
-		"<rootDir>/test/app_test.js",
-	],
+	testMatch: ["<rootDir>/test/**/*_test.js"],
 	setupFilesAfterEnv: ["jest-extended/all"],
 };
 
--- test/integration/game_test.js
+++ test/integration/game_test.js
@@ -1,13 +1,71 @@
-const chai = require('chai');
-const expect = chai.expect;
+const { when } = require("jest-when");
+const { readFileSync, writeFileSync } = require("fs");
+const app = require("../../src/app");
+const gameState = require("../../src/league");
 
-const app = require('../../src/app');
-const gameState = require('../../src/league');
+jest.mock("fs");
 
-describe('league app', function () {
-  it('prints empty game state', function () {
-    const game = app.startGame(gameState.createLeague());
+afterEach(() => {
+	jest.resetAllMocks();
+});
 
-    expect(game.sendCommand('print')).to.equal('No players yet');
-  });
+test("prints empty game state", () => {
+	const game = app.startGame(gameState.createLeague());
+
+	expect(game.sendCommand("print")).toEqual("No players yet");
+});
+
+test("prints game state with players", () => {
+	const game = app.startGame(gameState.createLeague());
+
+	game.sendCommand("add player Alice");
+	game.sendCommand("add player Bob");
+
+	expect(game.sendCommand("print")).toEqual(`          -------------------
+          |      Alice      |
+          -------------------
+------------------- -------------------
+|       Bob       | |                 |
+------------------- -------------------`);
+});
+
+test("records and returns winner", () => {
+	const game = app.startGame(gameState.createLeague());
+
+	game.sendCommand("add player Alice");
+	game.sendCommand("add player Bob");
+	game.sendCommand("record win Bob Alice");
+
+	expect(game.sendCommand("winner")).toEqual("Bob");
+});
+
+test("prints game state after loading", () => {
+	const loadPath = "some/file/path.json";
+	when(readFileSync)
+		.calledWith(loadPath, expect.any(String))
+		.mockReturnValue('[["Alice"], ["Bob"]]')
+		.defaultImplementation((path) => {
+			throw new Error(`File ${path} does not exist`);
+		});
+	const game = app.startGame(gameState.createLeague());
+
+	game.sendCommand(`load ${loadPath}`);
+
+	expect(game.sendCommand("print")).toEqual(`          -------------------
+          |      Alice      |
+          -------------------
+------------------- -------------------
+|       Bob       | |                 |
+------------------- -------------------`);
+});
+
+test("saves game state to file", () => {
+	const savePath = "some/file/path.json";
+	const game = app.startGame(gameState.createLeague());
+
+	game.sendCommand("add player Alice");
+	game.sendCommand("add player Bob");
+	game.sendCommand(`save ${savePath}`);
+
+	expect(writeFileSync).toHaveBeenCalledWith(savePath, JSON.stringify([["Alice"], ["Bob"]]), { flag: "w" });
 });
